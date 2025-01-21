8b3816398c1b49396ef310872a1947f174a0ead2
Brian Purnomo <brianbbs98@gmail.com>
2025-01-21 00:09:52 -0500

    Add ability to cut clips in between, currently hard cuts

--- app.py
+++ app.py
@@ -31,19 +31,69 @@ def process_file(filename):
     if request.method == 'POST':
         start_time = float(request.form['start_time'])
         end_time = float(request.form['end_time'])
+        clip_start = request.form.getlist('clip_start[]')
+        clip_end = request.form.getlist('clip_end[]')
+        print(clip_start)
+        print(type(clip_start))
+        print(len(clip_start))
+        print(clip_end)
         cross_dissolve_duration = 1  # Duration of the cross dissolve effect in seconds
         output_video_path = os.path.join(PROCESSED_FOLDER, f"clipped_{filename}")
         output_audio_path = os.path.join(PROCESSED_FOLDER, f"{os.path.splitext(filename)[0]}.mp3")
-        
-        # Clip video with cross dissolve effect
-        input_video = ffmpeg.input(filepath, ss=start_time, to=end_time)
-        video = (
-            input_video
-            .filter('fade', type='in', start_time=0, duration=cross_dissolve_duration)
-            .filter('fade', type='out', start_time=end_time-start_time-cross_dissolve_duration, duration=cross_dissolve_duration)
-        )
-        audio = input_video.audio
-        ffmpeg.output(video, audio, output_video_path).overwrite_output().run()
+
+        if len(clip_start) > 0 and len(clip_end) > 0 and len(clip_start) == len(clip_end):
+           clip_start = [float(x) for x in clip_start]
+           clip_end = [float(x) for x in clip_end]
+        else :
+            clip_start = None
+            clip_end = None
+
+        # get number of segments
+        num_segments = 1
+        if clip_start and clip_end:
+            num_segments = len(clip_start) + 1
+
+        # Load input
+        input_file = ffmpeg.input(filepath, ss=start_time, to=end_time)
+
+        if num_segments == 1:
+            final_video = (input_file.video
+                 .filter('fade', type='in', start_time=0, duration=cross_dissolve_duration)
+                 .filter('fade', type='out',
+                         start_time=(end_time - start_time - cross_dissolve_duration),
+                         duration=cross_dissolve_duration)
+                )
+            final_audio = (input_file.audio
+                 .filter('afade', t='in', st=0, d=cross_dissolve_duration)
+                 .filter('afade', t='out',
+                         st=(end_time - start_time - cross_dissolve_duration),
+                         d=cross_dissolve_duration)
+                )
+        else:
+            input_video_s = input_file.video
+            input_audio_s = input_file.audio
+
+            final_video_seg = []
+            final_audio_seg = [] 
+            for i in range(num_segments):
+                if i == 0:
+                    final_video_seg.append(input_video_s.filter('trim', start=start_time, end=clip_start[0]).filter('setpts', 'PTS-STARTPTS'))
+                    final_audio_seg.append(input_audio_s.filter('atrim', start=start_time, end=clip_start[0]).filter('asetpts', 'PTS-STARTPTS'))
+                elif i == num_segments - 1:
+                    final_video_seg.append(input_video_s.filter('trim', start=clip_end[i-1], end=end_time).filter('setpts', 'PTS-STARTPTS'))
+                    final_audio_seg.append(input_audio_s.filter('atrim', start=clip_end[i-1], end=end_time).filter('asetpts', 'PTS-STARTPTS'))
+                else:
+                    final_video_seg.append(input_video_s.filter('trim', start=clip_end[i-1], end=clip_start[i]).filter('setpts', 'PTS-STARTPTS'))
+                    final_audio_seg.append(input_audio_s.filter('atrim', start=clip_end[i-1], end=clip_start[i]).filter('asetpts', 'PTS-STARTPTS'))
+            
+            # Concatenate video and audio
+            print(final_video_seg)
+            print(final_audio_seg)
+            print(num_segments)
+            final_video = ffmpeg.concat(*final_video_seg, n=num_segments, v=1, a=0)
+            final_audio = ffmpeg.concat(*final_audio_seg, n=num_segments, v=0, a=1)
+
+        ffmpeg.output(final_video, final_audio, output_video_path).overwrite_output().run()
         
         # Extract audio
         ffmpeg.input(output_video_path).output(output_audio_path, acodec='libmp3lame').overwrite_output().run()
